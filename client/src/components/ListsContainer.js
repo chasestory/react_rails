179ae57586782c10be57b495a7ccb44e33a082c3
chasestory <chasestory@outlook.com>
2019-07-29 13:39:16 -0600

    got api data passing effectively through back-front, and init first component listsContainer

---
+++ client/src/components/ListsContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import "../styles/ListsContainer.css";
+
+class ListsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists')
+      .then(response => {
+        console.log(response)
+        this.setState({
+          lists: response.data
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="Lists-container">
+        {this.state.lists.map(list => {
+          return (
+            <div className="single-list" key={list.id}>
+              <h4>{list.Title}</h4>
+              <p>{list.Excerpt}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default ListsContainer;
